fe36b38f710239d377b2f61b8e1e3501dbda9430
Dhruv Sharda <135603782+DhruvSharda@users.noreply.github.com>
2026-09-18 12:24:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21.14%) - LeetHub-Neo

---
+++ leet/0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 1)
+            return false;
+
+        int sum = 1;
+        int sq = sqrt(num);
+
+        for (int i = 2; i <= sq; i++) {
+            if (num % i == 0) {
+                sum += i;
+
+                if (i != num / i)
+                    sum += num / i;
+            }
+        }
+
+        return sum == num;
+    }
+};
